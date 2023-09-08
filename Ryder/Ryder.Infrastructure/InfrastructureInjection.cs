c84b1acde3e42b6e6649c250d62f1a94aaf50fbf
John Kehinde <kehindejohn018@gmail.com>
2023-09-08 09:25:53 +0100

    added token and current user service

---
+++ Ryder/Ryder.Infrastructure/InfrastructureInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Ryder.Infrastructure.Implementation;
+using Ryder.Infrastructure.Interface;
+
+namespace Ryder.Infrastructure
+{
+    public static class InfrastructureInjection
+    {
+        public static void InjectInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ITokenGeneratorService, ITokenGeneratorService>();
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+    }
+}
