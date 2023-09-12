21634131d25d1551fc19ed50998a29a0ce738b51
imadetech1 <133721330+imadetech1@users.noreply.github.com>
2023-09-12 20:17:20 +0100

    Reset and Forget Password Api Done

---
+++ Ryder/Ryder.Application/User/Query/ResetPassword/ResetPasswordCommandHandler.cs
@@ -0,0 +1,64 @@
+﻿using AspNetCoreHero.Results;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Ryder.Application.User.Command.ResetPassword;
+using Ryder.Domain.Entities;
+using Ryder.Infrastructure.Interface;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ryder.Application.User.Query.ResetPassword
+{
+    public class ResetPasswordCommandHandler : IRequestHandler<ResetPasswordCommand, IResult<ResetPasswordResponse>>
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ISmtpEmailService _emailService;
+
+        public ResetPasswordCommandHandler(UserManager<AppUser> userManager, ISmtpEmailService emailService)
+        {
+            _userManager = userManager;
+            _emailService = emailService;
+        }
+
+        public async Task<IResult<ResetPasswordResponse>> Handle(ResetPasswordCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                // Validate the reset token
+                var user = await _userManager.FindByEmailAsync(request.Email);
+                if (user == null)
+                    return Result<ResetPasswordResponse>.Fail("User not found.");
+
+                var decodedToken = WebUtility.UrlDecode(request.ResetToken);
+                var result = await _userManager.ResetPasswordAsync(user, decodedToken, request.NewPassword);
+                if (!result.Succeeded)
+                    return Result<ResetPasswordResponse>.Fail("Failed to reset the password.");
+
+                // Send a password reset email using your IEmailService
+                var emailSubject = "Password Reset Successful";
+                var emailMessage = "Your password has been successfully reset.";
+
+                var emailSent = await _emailService.SendEmailAsync(request.Email, emailSubject, emailMessage);
+                if (!emailSent)
+                    return Result<ResetPasswordResponse>.Fail("Failed to send password reset email.");
+
+                var response = new ResetPasswordResponse
+                {
+                    Email = request.Email,
+                    NewPassword = request.NewPassword,
+                    ResetToken = request.ResetToken
+                };
+
+                return Result<ResetPasswordResponse>.Success(response, "Password reset successful.");
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions, log details, and return an appropriate error result.
+                return Result<ResetPasswordResponse>.Fail($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Ryder/Ryder.Infrastructure/Implementation/SmtpEmailService.cs
@@ -0,0 +1,49 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using Ryder.Infrastructure.Interface;
+using Ryder.Infrastructure.Common.Extensions;
+
+
+namespace Ryder.Infrastructure.Implementation
+{
+    public class SmtpEmailService : ISmtpEmailService
+    {
+        private readonly EmailSettings _settings;
+
+        public SmtpEmailService(EmailSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public async Task<bool> SendEmailAsync(string toEmail, string subject, string message)
+        {
+            try
+            {
+                var email = new MimeMessage();
+                email.From.Add(new MailboxAddress(_settings.SenderName, _settings.SmtpUsername));
+                email.To.Add(new MailboxAddress("", toEmail));
+                email.Subject = subject;
+
+                var body = new TextPart("plain")
+                {
+                    Text = message
+                };
+
+                var multipart = new Multipart { body };
+                email.Body = multipart;
+
+                using var client = new SmtpClient();
+                await client.ConnectAsync(_settings.SmtpHost, _settings.SmtpPort, true);
+                await client.AuthenticateAsync(_settings.SenderName, _settings.SmtpPassword);
+                await client.SendAsync(email);
+                await client.DisconnectAsync(true);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
