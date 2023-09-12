dfdeaf4e39f7f941622372bb6f71891985a598fc
Lucky Otono <51697000+otono-lucky@users.noreply.github.com>
2023-09-12 10:32:55 +0100

    created the endpoint to get ride history by id

---
+++ Ryder/Ryder.Api/Controllers/RideHistoryController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Ryder.Application.AllRiderHistory.Query;
+using Ryder.Domain.Entities;
+
+namespace Ryder.Api.Controllers
+{
+    [Route("api/ridehistorybyid")]
+    [ApiController]
+    public class RideHistoryController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet("ride-history-by-id/{riderId}")]
+        public async Task<IActionResult> GetRideHistoryById(Guid riderId)
+        {
+            
+            return await Initiate(() => Mediator.Send(new RideHistoryQuery { RiderId = riderId }));
+        }
+    }
+}
---
+++ Ryder/Ryder.Application/AllRiderHistory/Query/RideHistoryQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using AspNetCoreHero.Results;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Ryder.Domain.Context;
+using Ryder.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryder.Application.AllRiderHistory.Query
+{
+    public class RideHistoryQueryHandler : IRequestHandler<RideHistoryQuery, IResult<IList<Order>>>
+    {
+        private readonly ApplicationContext _context;
+        private readonly IMapper _mapper;
+
+        public RideHistoryQueryHandler(ApplicationContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task <IResult<IList<Order>>> Handle(RideHistoryQuery request, CancellationToken cancellationToken)
+        {
+            var rideHistory = await _context.Orders
+            .Where(r => r.RiderId == request.RiderId)
+            .ToListAsync();
+
+            if (rideHistory == null || !rideHistory.Any())
+            {
+                return await Result<List<Order>>.FailAsync();
+            }
+
+            var rideHistoryDTOs = _mapper.Map<Result<List<Order>>>(rideHistory);
+            return rideHistoryDTOs;
+        }
+    }
+}
