207904a85ba7535dab4e077bd43ec593a016c7f5
Estarmani <zamaniesther@gmail.com>
2023-09-12 12:19:11 +0100

    Created end point for sign up as a user and a rider

---
+++ Ryder/Ryder.Application/SignUp/Command/CreateRider/CreateRiderCommand.cs
@@ -0,0 +1,37 @@
+﻿using AspNetCoreHero.Results;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryder.Application.SignUp.Command.CreateRider
+{
+    public class CreateRiderCommand : IRequest<IResult>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string ValidIdUrl { get; set; }
+        public string PassportPhoto { get; set; }
+        public string BikeDocument { get; set; }
+    }
+    public class CreateRiderCommandValidator : AbstractValidator<CreateRiderCommand>
+    {
+        public CreateRiderCommandValidator()
+        {
+            //Validate properties
+            RuleFor(x => x.FirstName).NotEmpty().Matches("^[A-Z][a-zA-Z]*$").WithMessage("Invalid name Format");
+            RuleFor(x => x.LastName).NotEmpty().Matches("^[A-Z][a-zA-Z]*$").WithMessage("Invalid name Format");
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Invalid Email address Format");
+            RuleFor(x => x.PhoneNumber).NotEmpty().MaximumLength(11).WithMessage("Invalid Phone number");
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(8).Matches("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])$").WithMessage("Invalid Password, must have 'ABCabcd723#$@'");
+            RuleFor(x => x.ValidIdUrl).NotNull().NotEmpty().WithMessage("Id cannot be empty");
+            RuleFor(x => x.BikeDocument).NotEmpty().NotNull().WithMessage("Document cannot be empty");
+        }
+    }
+}
---
+++ Ryder/Ryder.Application/SignUp/Command/CreateRider/CreateRiderCommandHandler.cs
@@ -0,0 +1,66 @@
+﻿using AspNetCoreHero.Results;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Ryder.Domain.Context;
+using Ryder.Domain.Entities;
+using Ryder.Infrastructure.Policy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Ryder.Application.SignUp.Command.CreateRider
+{
+    public class CreateRiderCommandHandler : IRequestHandler<CreateRiderCommand, IResult>
+    {
+        private readonly ApplicationContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly UserManager<Rider> _manager;
+        public CreateRiderCommandHandler(ApplicationContext context, UserManager<AppUser> userManager, UserManager<Rider> manager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _manager = manager;
+        }
+        public async Task<IResult> Handle(CreateRiderCommand request, CancellationToken cancellationToken)
+        {
+            var createRider = await _userManager.FindByEmailAsync(request.Email);
+            if (createRider != null) return Result.Fail("Rider exist");
+            createRider = new Domain.Entities.AppUser()
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+                PasswordHash = request.Password,
+                UserName = request.Email
+            };
+
+            var riderDocumentation = await _manager.FindByIdAsync(request.ValidIdUrl);
+            if (riderDocumentation != null) return Result.Fail("Document already exist");
+            riderDocumentation = new Domain.Entities.Rider()
+            {
+                ValidIdUrl = request.ValidIdUrl,
+                PassportPhoto = request.PassportPhoto,
+                BikeDocument = request.BikeDocument
+            };
+
+            using(var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var createdRider = await _userManager.CreateAsync(createRider, request.Password);
+                if (!createdRider.Succeeded) return Result.Fail();
+                var riderDocuments = await _manager.CreateAsync(riderDocumentation);
+                if (!riderDocuments.Succeeded) return Result.Fail();
+                var CreateRole = await _userManager.AddToRoleAsync(createRider, Policies.Rider);
+                if (!CreateRole.Succeeded) return Result.Fail();
+                transaction.Complete();
+                await _context.SaveChangesAsync(cancellationToken);
+                return Result.Success("Signup successful");
+
+
+            }
+        }
+    }
+}
