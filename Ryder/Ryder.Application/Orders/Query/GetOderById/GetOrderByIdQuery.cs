4094b5b4ece72d5113abcde34b0f89fd6fe1546d
Batowei Arepua Michael <113602826+batoweiAM@users.noreply.github.com>
2023-09-13 23:07:00 +0100

    APIs to placeorder

---
+++ Ryder/Ryder.Application/Orders/Query/GetOderById/GetOrderByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using AspNetCoreHero.Results;
+using MediatR;
+using Ryder.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryder.Application.Orders.Query.GetOderById
+{
+    public class GetOrderByIdQuery : IRequest<IResult<Order>>
+    {
+        public Guid OrderId { get; set; }
+    }
+}
