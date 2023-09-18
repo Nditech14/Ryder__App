f5bef8a4e52f221590b8497b1e4ccccb01062a93
Finejojo <108051271+Finejojo@users.noreply.github.com>
2023-09-18 14:55:19 +0100

    Resolved controller

--- Ryder/Ryder.Api/Controllers/RiderController.cs
+++ Ryder/Ryder.Api/Controllers/RiderController.cs
@@ -5,23 +5,16 @@ using Ryder.Api.Controllers;
 using Ryder.Application.RiderAvailability.Command;
 using Ryder.Application.RiderAvailability.Query;
 
-[ApiController]
-[Route("api/riders")]
-public class RidersController : ApiController
+
+[AllowAnonymous]
+public class RiderController : ApiController
 {
-    private readonly IMediator _mediator;
-
-    public RidersController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     
-    [HttpPost("update-availability")]
-    public async Task<IActionResult> UpdateRiderAvailability(Guid id, [FromBody] UpdateRiderAvailabilityCommand command)
+    [HttpPost("update-availability/{id}")]
+    public async Task<IActionResult> UpdateRiderAvailability(Guid id, UpdateRiderAvailabilityCommand command)
     {
-
-        return await Initiate(() => Mediator.Send(new UpdateRiderAvailabilityCommand { RiderId = id }));
+        command.RiderId = id;
+        return await Initiate(() => Mediator.Send(command));
       
        
     }
@@ -30,9 +23,7 @@ public class RidersController : ApiController
     [HttpGet("get-availability/{id}")]
     public async Task<IActionResult> GetRiderAvailability(Guid id)
     {
-        var query = new GetRiderAvailabilityQuery { RiderId = id };
-        return await Initiate(() => Mediator.Send(query));
-
+        return await Initiate(() => Mediator.Send(new GetRiderAvailabilityQuery { RiderId = id }));
     }
 
     
--- Ryder/Ryder.Application/RiderAvailability/Command/UpdateRiderAvailabilityCommand.cs
+++ Ryder/Ryder.Application/RiderAvailability/Command/UpdateRiderAvailabilityCommand.cs
@@ -5,13 +5,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Ryder.Application.RiderAvailability.Command
 {
-   
     public class UpdateRiderAvailabilityCommand : IRequest<IResult<RiderAvailabilityResponse>>
     {
+        [JsonIgnore]
         public Guid RiderId { get; set; }
         public RiderAvailabilityStatus AvailabilityStatus { get; set; }
     }
