248fa9eae79c0df553932f29f94f08b881116d76
Finejojo <108051271+Finejojo@users.noreply.github.com>
2023-09-18 14:55:44 +0100

    Resolved handler

--- Ryder/Ryder.Api/Controllers/RiderController.cs
+++ Ryder/Ryder.Api/Controllers/RiderController.cs
@@ -5,26 +5,18 @@ using Ryder.Api.Controllers;
 using Ryder.Application.RiderAvailability.Command;
 using Ryder.Application.RiderAvailability.Query;
 
-
-[AllowAnonymous]
 public class RiderController : ApiController
 {
-    
     [HttpPost("update-availability/{id}")]
     public async Task<IActionResult> UpdateRiderAvailability(Guid id, UpdateRiderAvailabilityCommand command)
     {
         command.RiderId = id;
         return await Initiate(() => Mediator.Send(command));
-      
-       
     }
 
-    
     [HttpGet("get-availability/{id}")]
     public async Task<IActionResult> GetRiderAvailability(Guid id)
     {
         return await Initiate(() => Mediator.Send(new GetRiderAvailabilityQuery { RiderId = id }));
     }
-
-    
 }
