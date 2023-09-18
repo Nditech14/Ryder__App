59c1c9a14b2681aefce5fd9efa7ef0d081754e6d
Dominic Precious <131398262+Nditech14@users.noreply.github.com>
2023-09-18 13:47:39 +0100

    removed dead codes

--- Ryder/Ryder.Api/Controllers/OrderController.cs
+++ Ryder/Ryder.Api/Controllers/OrderController.cs
@@ -1,59 +1,63 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Ryder.Application.Orders.Command.PlaceOrder;
 using Ryder.Application.Orders.Query.GetAllOrder;
-using Ryder.Application.Orders.Query.GetOderById;
+using Microsoft.Extensions.Logging;
 using Ryder.Application.order.Query.AcceptOrder;
 using Ryder.Application.order.Query.EndRide;
 using Ryder.Application.order.Query.OrderProgress;
-using Microsoft.Extensions.Logging; // Import the logging library.
+using Ryder.Application.Orders.Query.GetOderById;
 
 namespace Ryder.Api.Controllers
 {
     public class OrderController : ApiController
     {
-        [HttpPost]
-        public async Task<IActionResult> PlaceOrder([FromBody] PlaceOrderCommand placeOrder)
-        private readonly ILogger<OrderController> _logger; // Inject the logger.
+        private readonly ILogger<OrderController> _logger;
 
         public OrderController(ILogger<OrderController> logger)
         {
-            return await Initiate(() => Mediator.Send(placeOrder));
             _logger = logger;
-
-            // Log an information message when the controller is initialized.
             _logger.LogInformation("OrderController initialized.");
+        }
+
+        [HttpPost("placeOrder")]
+        public async Task<IActionResult> PlaceOrder([FromBody] PlaceOrderCommand placeOrder)
+        {
+            
+            return await Initiate(() => Mediator.Send(placeOrder));
         }
 
         [HttpPost("accept")]
         public async Task<IActionResult> AcceptOrder([FromBody] AcceptOrderCommand command)
         {
-            // Log an information message when the AcceptOrder action is invoked.
             _logger.LogInformation("AcceptOrder action invoked.");
-
             return await Initiate(() => Mediator.Send(command));
         }
 
-        [HttpGet]
+        [HttpGet("getAllOrder")]
         public async Task<IActionResult> GetAllOrder()
+        {
+            
+            return await Initiate(() => Mediator.Send(new GetAllOrderQuery()));
+        }
+
         [HttpPost("progress")]
         public async Task<IActionResult> RequestProgress([FromBody] OrderProgressCommand command)
         {
-            return await Initiate(() => Mediator.Send(new GetAllOrderQuery()));
-            // Log an information message when the RequestProgress action is invoked.
             _logger.LogInformation("RequestProgress action invoked.");
-
             return await Initiate(() => Mediator.Send(command));
         }
 
         [HttpGet("{orderId}")]
         public async Task<IActionResult> GetOrderById(Guid orderId)
+        {
+            
+            return await Initiate(() => Mediator.Send(new GetOrderByIdQuery { OrderId = orderId }));
+        }
+
         [HttpPost("end")]
         public async Task<IActionResult> EndRide([FromBody] EndRideCommand command)
         {
-            return await Initiate(() => Mediator.Send(new GetOrderByIdQuery { OrderId = orderId }));
-            // Log an information message when the EndRide action is invoked.
             _logger.LogInformation("EndRide action invoked.");
-
             return await Initiate(() => Mediator.Send(command));
         }
     }
