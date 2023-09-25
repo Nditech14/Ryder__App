63e859862ebb40a095f498d3c031efdbc66cb72b
James Adah <adahjames11@gmail.com>
2023-09-25 14:19:45 +0100

    change

--- Ryder/Ryder.Api/Controllers/UserController.cs
+++ Ryder/Ryder.Api/Controllers/UserController.cs
@@ -14,12 +14,9 @@ namespace Ryder.Api.Controllers
             return await Initiate(() => Mediator.Send(new GetCurrentUserCommand()));
         }
 
-        [AllowAnonymous]
         [HttpGet("GetUserProfile/{id}")]
         public async Task<IActionResult> GetUserProfileAsync(string id)
         {
-            //var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var userId = "7cd2e636-df08-4e3c-95e6-a337808037bb";
             return await Initiate(() => Mediator.Send(new GetUserProfileQuery(id)));
         }
     }
