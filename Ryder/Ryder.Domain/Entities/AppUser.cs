0a2970cfa68ab97588b80de37021b349a6f68064
David Ogwuche <133539148+codedvd@users.noreply.github.com>
2023-09-25 11:04:20 +0100

    Updated SMTP Settings

--- Ryder/Ryder.Domain/Entities/AppUser.cs
+++ Ryder/Ryder.Domain/Entities/AppUser.cs
@@ -7,7 +7,7 @@ namespace Ryder.Domain.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? ProfilePictureUrl { get; set; }
-        public string RefreshToken { get; set; } 
+        public string? RefreshToken { get; set; } 
         public DateTime RefreshTokenExpiryTime { get; set; }
         public Address? Address { get; set; }
     }
