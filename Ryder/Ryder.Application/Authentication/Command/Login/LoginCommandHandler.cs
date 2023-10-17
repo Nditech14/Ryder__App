96737e330b8f3fa237a5ab9a74778d18a44fd7bd
Mistaweng <emmanuelgyang.1822@gmail.com>
2023-10-17 17:09:23 +0100

    Resolving merge conflicts

--- Ryder/Ryder.Application/Authentication/Command/Login/LoginCommandHandler.cs
+++ Ryder/Ryder.Application/Authentication/Command/Login/LoginCommandHandler.cs
@@ -1,7 +1,9 @@
 ﻿using AspNetCoreHero.Results;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Ryder.Domain.Context;
 using Ryder.Domain.Entities;
 using Ryder.Infrastructure.Interface;
 using System.Text;
@@ -10,20 +12,23 @@ using System.Threading.Tasks;
 
 namespace Ryder.Application.Authentication.Command.Login
 {
-    public class LoginCommandHandler : IRequestHandler<LoginCommand, IResult<LoginResponse>>
-    {
-        private readonly IUserService _userService;
-        private readonly ITokenGeneratorService _tokenService;
-        private readonly IConfiguration _configuration;
+	public class LoginCommandHandler : IRequestHandler<LoginCommand, IResult<LoginResponse>>
+	{
+		private readonly IUserService _userService;
+		private readonly ITokenGeneratorService _tokenService;
+		private readonly IConfiguration _configuration;
 		private readonly UserManager<AppUser> _userManager;
+		private readonly ApplicationContext _context;
 
-        public LoginCommandHandler(IUserService userService, ITokenGeneratorService tokenService, IConfiguration configuration, UserManager<AppUser> userManager)
-        {
-            _userService = userService;
-            _tokenService = tokenService;
-            _configuration = configuration;
+		public LoginCommandHandler(IUserService userService, ITokenGeneratorService tokenService, IConfiguration configuration, UserManager<AppUser> userManager, ApplicationContext context)
+		{
+			_userService = userService;
+			_tokenService = tokenService;
+			_configuration = configuration;
 			_userManager = userManager;
-        }
+			_context = context;
+		}
+
 
 		public async Task<IResult<LoginResponse>> Handle(LoginCommand request, CancellationToken cancellationToken)
 		{
@@ -35,11 +40,19 @@ namespace Ryder.Application.Authentication.Command.Login
 				return Result<LoginResponse>.Fail("Invalid username or password");
 			}
 
-			// Generate token
-			var token = await _tokenService.GenerateTokenAsync(user);
-
 			// Get the user's roles
 			var userRoles = await _userManager.GetRolesAsync(user);
+
+			var query = from rider in _context.Riders
+						join
+						appUser in _context.Users on
+						rider.AppUserId equals appUser.Id
+						where appUser.Id == user.Id
+						select rider.Id;
+			var riderId = query.FirstOrDefault();
+
+			
+			var token = await _tokenService.GenerateTokenAsync(user);
 
 			var response = new LoginResponse
 			{
@@ -47,8 +60,12 @@ namespace Ryder.Application.Authentication.Command.Login
 				UserId = user.Id,
 				UserName = user.UserName,
 				FullName = $"{user.FirstName} {user.LastName}",
-				UserRole = userRoles.FirstOrDefault()
-														
+				UserRole = userRoles.FirstOrDefault(),
+				RiderId = riderId
+				
+				
+				//RiderId = riderId.AppUserId
+
 			};
 
 			return Result<LoginResponse>.Success(response);
@@ -56,32 +73,7 @@ namespace Ryder.Application.Authentication.Command.Login
 
 
 
-		//     public async Task<IResult<LoginResponse>> Handle(LoginCommand request, CancellationToken cancellationToken)
-		//     {
-		//         // Validate user credentials
-		//         var user = await _userService.ValidateUserAsync(request.Email, request.Password);
 
-		//         if (user == null)
-		//         {
-		//             return Result<LoginResponse>.Fail("Invalid username or password");
-		//         }
-
-		//         // Generate token
-		//         var token = await _tokenService.GenerateTokenAsync(user);
-
-		//         var response = new LoginResponse
-		//         {
-		//             Token = token,
-		//             UserId = user.Id,
-		//             UserName = user.UserName,
-		//             FullName = $"{user.FirstName} {user.LastName}",
-
-
-		//	// Add any other properties you want to return on successful login
-		//};
-
-		//         return Result<LoginResponse>.Success(response);
-		//     }
 	}
 }
 
--- Ryder/Ryder.Application/Authentication/Command/Login/LoginResponse.cs
+++ Ryder/Ryder.Application/Authentication/Command/Login/LoginResponse.cs
@@ -13,17 +13,8 @@ namespace Ryder.Application.Authentication.Command.Login
 		public string FullName { get; set; } // User's full name
 		public string Token { get; set; } // Authentication token (JWT) for subsequent requests
 		public string UserRole { get; set; } // User's role
-											 
-	}
+        public Guid RiderId { get; set; }
 
+    }
 
-
-	//   public class LoginResponse
-	//   {
-	//       public Guid UserId { get; set; } // Unique identifier for the user
-	//       public string UserName { get; set; } // User's username or email
-	//       public string FullName { get; set; } // User's full name
-	//       public string Token { get; set; } // Authentication token (JWT) for subsequent requests
-	//	// Additional properties as needed (e.g., UserRole, UserEmail, etc.)
-	//}
 }
