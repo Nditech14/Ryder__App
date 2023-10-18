22e4af0a8035472ec471b259d2f1886f58f6f28d
David Ogwuche <133539148+codedvd@users.noreply.github.com>
2023-10-18 13:33:18 +0100

    Commit

--- Ryder/Ryder.Application/Authentication/Command/Login/LoginResponse.cs
+++ Ryder/Ryder.Application/Authentication/Command/Login/LoginResponse.cs
@@ -8,11 +8,11 @@ namespace Ryder.Application.Authentication.Command.Login
 {
 	public class LoginResponse
 	{
-		public Guid UserId { get; set; } // Unique identifier for the user
-		public string UserName { get; set; } // User's username or email
-		public string FullName { get; set; } // User's full name
-		public string Token { get; set; } // Authentication token (JWT) for subsequent requests
-		public string UserRole { get; set; } // User's role
+		public Guid UserId { get; set; }
+		public string UserName { get; set; }
+		public string FullName { get; set; }
+		public string Token { get; set; } 
+		public string UserRole { get; set; }
 											 
 	}
 
